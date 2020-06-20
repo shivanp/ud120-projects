a25e84f8e657bc312bd25c01d1c78572b5947862
shivanp <47057171+shivanp@users.noreply.github.com>
2020-06-20 16:06:21 +0100

    Revert "python 3 bug fixed on preprocessing of data (project 1)"
    This reverts commit 35e9ba381388653ae6dd70f2319cfc1d76cc4be2.

--- tools/email_preprocess.py
+++ tools/email_preprocess.py
@@ -3,16 +3,15 @@
 import pickle
 import numpy
 
-from sklearn.model_selection import cross_validate
-from sklearn.model_selection import train_test_split
+from sklearn import cross_validation
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_selection import SelectPercentile, f_classif
 
 
 
-def preprocess(words_file = "../tools/word_data_unix.pkl", authors_file="../tools/email_authors.pkl"):
+def preprocess(words_file = "../tools/word_data.pkl", authors_file="../tools/email_authors.pkl"):
     """ 
-        this function takes a pre-made list of email texts (by default word_data_unix.pkl)
+        this function takes a pre-made list of email texts (by default word_data.pkl)
         and the corresponding authors (by default email_authors.pkl) and performs
         a number of preprocessing steps:
             -- splits into training/testing sets (10% testing)
@@ -38,7 +37,7 @@ def preprocess(words_file = "../tools/word_data_unix.pkl", authors_file="../tool
     words_file_handler.close()
 
     ### test_size is the percentage of events assigned to the test set (remainder go into training)
-    features_train, features_test, labels_train, labels_test = train_test_split(word_data, authors, test_size=0.1, random_state=42)
+    features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(word_data, authors, test_size=0.1, random_state=42)
 
 
 
